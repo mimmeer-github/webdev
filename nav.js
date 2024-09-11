08884366641257a29b86177f986fddcddf055990
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-09-11 16:20:17 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,20 +1,20 @@
-function includeHTML() {
+function includeHTML(file, id) {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain attribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute(id);
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 404) {elmnt.innerHTML = "Uh-Oh, this page doesn't exist!";}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute(id);
           includeHTML();
         }
       }      
